e19d64b1bec5b0b1ad14b043c8a1d873adeb758c
githubzuoyi <1783164557@qq.com>
2018-12-16 22:09:49 +0800

    创建项目

---
+++ jvm_notebook/src/test20181216_jvmruntime/java1.java
@@ -0,0 +1,65 @@
+package test20181216_jvmruntime;
+
+class SuperClass{
+	SuperClass(){
+		System.out.println("init_SuperClass2");
+	}
+	static {
+		System.out.println("init_SuperClass");
+	}
+}
+
+public class java1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//SuperClass s1 = new SuperClass();
+		
+		//代码清单7-4
+		//System.out.println(Sub.A);
+	}
+
+}
+
+//代码清单7-4
+class FieldResolution{
+	interface Interface0{
+		int A = 0;
+	}
+	
+	interface Interface1 extends Interface0{
+		int A = 3;
+	}
+	
+	interface Interface2{
+		int A = 2;
+	}
+	
+	static class Parent implements Interface1{
+		public static int A = 3;
+	}
+	
+	static class Sub extends Parent implements Interface2{
+		public static int A = 4;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//SuperClass s1 = new SuperClass();
+		
+		//代码清单7-4
+		System.out.println(Sub.A);
+	}
+}
+
+//代码清单7-6
+class DeadLoopClass{
+	static {
+		if(true) {
+			System.out.println("init_SuperClass");
+			while(true) {
+				
+			}
+		}
+	}
+}
